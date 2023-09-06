8b5713809b2e8bce2d7db5f3e87752d1fc5fe323
HerlynMae <torresherlynmae@gmail.com>
2023-09-06 20:55:46 +0800

    update

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const { link } = require("mz/fs");
+
+const starNav = document.querySelectorAll(".star__nav__link");
+const starContent = document.querySelectorAll(".coffee__tab__content");
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent = document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveStar() {
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  starContent.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
+
+
