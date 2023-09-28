3511fab75bf0eb25ac09f35f0b335331a499a261
aaairam <magildaceria@gmail.com>
2023-09-28 09:58:40 +0800

    tab and accordion

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
+const accordions = document.querySelectorAll(".accordion__item h4");
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.nextElementSibling.classList.toggle("open");
+    accordion.querySelector("i").classList.toggle("open");
+  });
+});
+
+// tab
 const starNav = document.querySelectorAll(".star__nav__link");
 const starContent = document.querySelectorAll(".coffee__tab__content");
-starNav.forEach((star) => { 
-    star.addEventListener("click", () => {
-      removeActiveStar();  
-      star.classList.add("active"); 
-      const activeContent = document.querySelector(`#${star.id}-content`);  
-      removeActiveContent();  
-      activeContent.classList.add("active"); 
-    });
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent = document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
 });
 
 function removeActiveStar() {
-     starNav.forEach((star) => {
-        star.classList.remove("active"); 
-    });
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  });
 }
 
-function removeActiveContent() { 
-    starContent.forEach((star) => { 
-         star.classList.remove("active");
-         });
-        }
-
-const accordionNav = document.querySelectorAll(".accordion__nav__link");
-const accordionContent = document.querySelectorAll(".accordion__content");
-accordionNav.forEach
+function removeActiveContent() {
+  starContent.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
